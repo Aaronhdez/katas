ca2ff5351c991d8346996dd0e87c6ad48618e519
Aaronhdez <aaronhdezalv@hotmail.com>
2022-10-03 15:13:44 +0100

    refactor: test refactored

--- Java/bowling_game_kata/src/test/java/LaneTest.java
+++ Java/bowling_game_kata/src/test/java/LaneTest.java
@@ -6,17 +6,17 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 public class LaneTest {
+    private Lane lane;
+
     @Before
     public void setUp(){
-
+        lane = new Lane();
     }
 
     @Test
     public void number_of_pins_is_10_on_each_round(){
-        Lane lane = new Lane();
         lane.loadARound();
         int pinsStandingAfter = lane.getPinsStanding();
-
         assertEquals(10, pinsStandingAfter);
     }
 
