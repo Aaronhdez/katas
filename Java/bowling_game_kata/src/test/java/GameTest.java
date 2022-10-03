c8de2ed3b4c66bba9ab6dd068a05a93d05827c70
Aaronhdez <aaronhdezalv@hotmail.com>
2022-10-03 16:55:11 +0100

    refactor: tests refactored to improve legibility

--- Java/bowling_game_kata/src/test/java/GameTest.java
+++ Java/bowling_game_kata/src/test/java/GameTest.java
@@ -8,36 +8,39 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class GameTest {
+    private Game game;
+    private Lane currentLane;
+    private Player currentPlayer;
+
     @Before
     public void setUp(){
-
+        game = new Game();
+        currentLane = game.getCurrentLane();
+        currentPlayer = game.getCurrentPlayer();
     }
 
     @Test
     public void game_must_have_only_one_lane(){
-        Game game = new Game();
-        Lane currentLane = game.getCurrentLane();
+        game = new Game();
         assertFalse(currentLane == null);
     }
 
     @Test
     public void game_must_have_only_one_player(){
-        Game game = new Game();
-        Player currentPlayer = game.getCurrentPlayer();
+        game = new Game();
         assertFalse(currentPlayer == null);
     }
 
     @Test
     public void game_sets_score_to_player_each_time_a_ball_is_rolled(){
-        Game game = new Game();
-        Player currentPlayer = game.getCurrentPlayer();
+        game = new Game();
         game.roll(2);
         assertEquals(game.score(), 2);
     }
 
     @Test
     public void frame_counter_should_be_increased_each_two_rolls(){
-        Game game = new Game();
+        game = new Game();
         game.roll(2);
         assertEquals(game.getCurrentFrame(), 1);
         game.roll(2);
@@ -45,17 +48,17 @@ public class GameTest {
     }
 
     @Test
-    public void player_should_receive_strike_status_if_a_strike_is_done(){
-        Game game = new Game();
+    public void frame_should_increase_if_a_strike_is_done(){
+        game = new Game();
         game.roll(10);
-        assertEquals(game.getCurrentPlayer().getStrike(), true);
+        assertEquals(game.getCurrentFrame(), 2);
     }
 
     @Test
-    public void frame_should_increase_if_a_strike_is_done(){
-        Game game = new Game();
+    public void player_should_receive_strike_status_if_a_strike_is_done(){
+        game = new Game();
         game.roll(10);
-        assertEquals(game.getCurrentFrame(), 2);
+        assertEquals(game.getCurrentPlayer().getStrike(), true);
     }
 
 }
