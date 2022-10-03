c30e2dd08ebbd35cb69528dda153313225d124d2
Aaronhdez <aaronhdezalv@hotmail.com>
2022-10-03 16:06:28 +0100

    green: game_must_have_only_one_lane

--- Java/bowling_game_kata/src/main/java/model/BowlingGame.java
+++ Java/bowling_game_kata/src/main/java/model/BowlingGame.java
@@ -1,7 +1,13 @@
 package model;
 
 public class BowlingGame {
+    private Lane currentLane;
+
+    public BowlingGame() {
+        this.currentLane = new Lane();
+    }
+
     public Lane getCurrentLane() {
-        return null;
+        return currentLane;
     }
 }
