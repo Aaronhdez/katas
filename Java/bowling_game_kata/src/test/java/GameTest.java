ee23b8dcee32fcdafd5ad42570f177279e323250
Aaronhdez <aaronhdezalv@hotmail.com>
2022-10-03 17:16:09 +0100

    refacotr: tests refactored

--- Java/bowling_game_kata/src/test/java/GameTest.java
+++ Java/bowling_game_kata/src/test/java/GameTest.java
@@ -21,26 +21,22 @@ public class GameTest {
 
     @Test
     public void game_must_have_only_one_lane(){
-        game = new Game();
         assertFalse(currentLane == null);
     }
 
     @Test
     public void game_must_have_only_one_player(){
-        game = new Game();
         assertFalse(currentPlayer == null);
     }
 
     @Test
     public void game_sets_score_to_player_each_time_a_ball_is_rolled(){
-        game = new Game();
         game.roll(2);
         assertEquals(game.score(), 2);
     }
 
     @Test
     public void frame_counter_should_be_increased_each_two_rolls(){
-        game = new Game();
         game.roll(2);
         assertEquals(game.getCurrentFrame(), 1);
         game.roll(2);
@@ -49,25 +45,23 @@ public class GameTest {
 
     @Test
     public void frame_should_increase_if_a_strike_is_done(){
-        game = new Game();
         game.roll(10);
         assertEquals(game.getCurrentFrame(), 2);
     }
 
     @Test
     public void player_should_receive_strike_status_if_a_strike_is_done(){
-        game = new Game();
         game.roll(10);
         assertEquals(game.getCurrentPlayer().getBonusRolls(), 2);
     }
 
     @Test
     public void player_should_receive_bonuses_if_a_strike_is_done(){
-        game = new Game();
         game.roll(10);
-        assertEquals(game.score(), 10);
+        int scoreBeforeStrike = game.score();
         game.roll(2);
         game.roll(2);
+        assertEquals(scoreBeforeStrike, 10);
         assertEquals(game.score(), 18);
     }
 
