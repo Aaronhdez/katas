71937044159b5c3a522cf6f6d9561897914db9dd
Aaronhdez <aaronhdezalv@hotmail.com>
2022-10-10 12:37:12 +0100

    green: Game_must_have_a_lane_assigned

--- C#/Bowling_Game/Model/Game.cs
+++ C#/Bowling_Game/Model/Game.cs
@@ -1,8 +1,10 @@
 ﻿namespace Bowling_Game.Model {
     public class Game {
         private Player player;
+        private Lane lane;
 
         public Player Player { get => player; set => player = value; }
+        public Lane Lane { get => lane; set => lane = value; }
 
         public void Roll(int pins) {
 
--- C#/Bowling_Game_Test/ModelTests/GameTests.cs
+++ C#/Bowling_Game_Test/ModelTests/GameTests.cs
@@ -16,5 +16,13 @@ namespace Bowling_Game_Test.ModelTests {
             game.Player = testPlayer;
             Assert.IsNotNull(game.Player);
         }
+
+        [Test]
+        public void Game_must_have_a_lane_assigned() {
+            game = new Game();
+            Lane testLane = new Lane();
+            game.Lane = testLane;
+            Assert.IsNotNull(game.Lane);
+        }
     }
 }
