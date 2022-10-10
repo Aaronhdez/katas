93d8b7bce7d16192c7776690af46077d65a4777a
Aaronhdez <aaronhdezalv@hotmail.com>
2022-10-10 12:19:46 +0100

    refactor: test variables extracted to setup method

--- C#/Bowling_Game_Test/ModelTests/PlayerTest.cs
+++ C#/Bowling_Game_Test/ModelTests/PlayerTest.cs
@@ -3,19 +3,20 @@ using Bowling_Game.Model;
 
 namespace Bowling_Game_Test.ModelTests {
     class PlayerTest {
+        private Player player;
+
         [SetUp]
         public void Setup() {
+            player = new Player();
         }
 
         [Test]
         public void Player_score_is_0_when_game_starts() {
-            Player player = new Player();
             Assert.AreEqual(0, player.GetScore());
         }
 
         [Test]
         public void Player_score_increases_when_at_least_one_pin_is_knocked_down() {
-            Player player = new Player();
             player.AddScore(1);
             Assert.AreEqual(1, player.GetScore());
         }
