b0a49907c3c0387b72737a2a303e4fad7dcfb3f3
Aaronhdez <aaronhdezalv@hotmail.com>
2022-10-10 11:34:42 +0100

    refactor: tests variables refactored

--- C#/Bowling_Game_Test/ModelTests/LaneTests.cs
+++ C#/Bowling_Game_Test/ModelTests/LaneTests.cs
@@ -3,13 +3,16 @@ using Bowling_Game.Model;
 
 namespace Bowling_Game_Test.ModelTests {
     public class LaneTests {
+        private Lane lane;
+
         [SetUp]
         public void Setup() {
+            lane = new Lane();
         }
 
         [Test]
         public void Lane_has_10_pins_standing_when_created() {
-            Lane lane = new Lane();
+            lane = new Lane();
             Assert.AreEqual(10, lane.GetPinsStanding());
         }
     }
