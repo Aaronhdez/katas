0f64617881fcd73a24e6b2cc3b04a8bda1ba3a7f
Aaronhdez <aaronhdezalv@hotmail.com>
2022-10-10 12:39:57 +0100

    refactor: tests refactored to include new constructor

--- C#/Bowling_Game_Test/ModelTests/GameTests.cs
+++ C#/Bowling_Game_Test/ModelTests/GameTests.cs
@@ -7,21 +7,18 @@ namespace Bowling_Game_Test.ModelTests {
 
         [SetUp]
         public void Setup() {
+            Player testPlayer = new Player();
+            Lane testLane = new Lane();
+            game = new Game(testPlayer, testLane);
         }
 
         [Test]
         public void Game_must_have_a_player_assigned() {
-            game = new Game();
-            Player testPlayer = new Player();
-            game.Player = testPlayer;
             Assert.IsNotNull(game.Player);
         }
 
         [Test]
         public void Game_must_have_a_lane_assigned() {
-            game = new Game();
-            Lane testLane = new Lane();
-            game.Lane = testLane;
             Assert.IsNotNull(game.Lane);
         }
     }
