13c84c93ad1fa3859396205e26d8317b9a76db95
Aaronhdez <aaronhdezalv@hotmail.com>
2022-10-31 11:16:26 +0000

    refactor: Importing data model refactored to allow straightforward serialization

--- C#/JsonSerialization/Model/ImportingData.cs
+++ C#/JsonSerialization/Model/ImportingData.cs
@@ -4,5 +4,26 @@
         public string Origin { get; set; }
         public string Destination { get; set; }
         public string Provider { get; set; }
+        public ImportingData() {
+        }
+
+        public ImportingData(string importingID, string origin, string destination, string provider) {
+            ImportingID = importingID;
+            Origin = origin;
+            Destination = destination;
+            Provider = provider;
+        }
+
+        public override bool Equals(object obj) {
+            return obj is ImportingData data &&
+                   ImportingID == data.ImportingID &&
+                   Origin == data.Origin &&
+                   Destination == data.Destination &&
+                   Provider == data.Provider;
+        }
+
+        public override int GetHashCode() {
+            return base.GetHashCode();
+        }
     }
 }
